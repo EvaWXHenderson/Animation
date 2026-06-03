e67e86b0f14a2beb4bdca102e99f65dcb3006520
Eva Henderson <eva.henderson01@gmail.com>
2026-06-03 21:46:13 +0100

    Update Animation.py
    Working animate() function
    Addition of closable and self-closing show() functions

--- Animation.py
+++ Animation.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation as an
 import matplotlib.colors as col
+import functools as fct
 
-def read(image, height, width):
+def read(image, frame, height, width):
     grid = []
 
-    for row in range(11, 10+height):
+    startline = 11 + (frame*34)
+    endline = (startline - 1) + height
+    
+    for row in range(startline, endline):
         row = image[row].split()
         for entry in range(0,width):
             row[entry] = row[entry].replace(",", "")
@@ -31,31 +36,74 @@ def rgb(grid):
 
     return rgb_grid
 
-def animate(file):
+def show_close(file):
     with open(file) as f:
         data = f.readlines()
 
     width_ = int(data[3][-3] + data[3][-2])
     height_ = int(data[4][-3] + data[4][-2])
     
-    grid = read(image = data, height=height_, width=width_)
+    grid = read(image = data, frame = 0, height=height_, width=width_)
     rgb_grid = rgb(grid)
 
     plt.ion()
 
     plt.style.use('_mpl-gallery-nogrid')
     fig, ax = plt.subplots(figsize = (5,5))
-    ax.imshow(rgb_grid)
 
     plt.xticks([]) 
     plt.yticks([])
 
+    ax.imshow(rgb_grid)
+
+def show(file):
+    with open(file) as f:
+        data = f.readlines()
+
+    width_ = int(data[3][-3] + data[3][-2])
+    height_ = int(data[4][-3] + data[4][-2])
+    
+    grid = read(image = data, frame = 0, height=height_, width=width_)
+    rgb_grid = rgb(grid)
+
+    plt.style.use('_mpl-gallery-nogrid')
+    fig, ax = plt.subplots(figsize = (5,5))
+
+    plt.xticks([]) 
+    plt.yticks([])
+
+    ax.imshow(rgb_grid)
+
+def animate(file, speed):
+    def grid(frame, width, height):
+        grid = read(image = data, frame = frame, height=height, width=width)
+        rgb_grid = rgb(grid)
+        ax.imshow(rgb_grid)
+
+    with open(file) as f:
+        data = f.readlines()
+    
+    frames = int((len(data)-11)/33)
+    width_ = int(data[3][-3] + data[3][-2]) #32
+    height_ = int(data[4][-3] + data[4][-2]) #32
+
+
+    plt.style.use('_mpl-gallery-nogrid')
+    fig, ax = plt.subplots(figsize = (5,5))
+
+    anim = an(fig, fct.partial(grid,width = width_,height = height_), frames = frames, interval = speed)
+
+    plt.xticks([]) 
+    plt.yticks([])
     plt.show()
 
 def close(image, command):
     input(command)
     plt.close(image)
 
+#image = animate(file = "Test_gif1.c", speed = 10)
 
-image = animate(file = "Piskel3.c")
-close(image, "press enter to close window")
+#image = show("Test_image1.c")
+
+#image = show_close("Test_image1.c"")
+#close(image, "press enter to close window") #issue - now need a close() line to have plot show
